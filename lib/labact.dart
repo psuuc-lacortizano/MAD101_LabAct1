252ccefb0b41155044a0a529e4f5d37271c4ab9a
psuuc-lacortizano <luizaldrin250@gmail.com>
2023-09-19 11:33:54 +0800

    init

---
+++ lib/labact.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+ // runApp(MaterialApp(
+
+   // debugShowCheckedModeBanner: false,
+
+//home: Scaffold(
+ // backgroundColor: Color.fromARGB(255, 133, 110, 180),
+
+//body: Center(
+ // child: Text("Oten ni renz mabaho"),
+//),
+
+//),
+
+ // ),
+  //;
+runApp(const Hihu());
+}
+
+class Hihu extends StatelessWidget {
+  const Hihu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("User's Information"),
+        ),
+        backgroundColor: Color.fromARGB(255, 252, 205, 205),
+        body: Container(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(top:20),
+                    child: Text("Student's Address: Macao, Killo Sison Pangasinan"),
+                  ),),
+
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(top:20),
+                      child: Text("Student's Address: Macao, Killo Sison Pangasinan"),
+                    ),),
+
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(top:20),
+                    child: Text("Student's Bio: A BSIT student from PSU Urdaneta, on a journey of continuous learning and innovation, harnessing the power of technology to shape a brighter and more connected future."),
+                  ),),
+
+
+
+
+              ],
+            ),
+          ),
+        ),
+
+
+      ),
+
+    );
+  }
+}
+
